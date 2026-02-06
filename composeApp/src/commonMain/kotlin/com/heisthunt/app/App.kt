e931c4a0bfc7f3113374c5fd4ed8c96fefde5c11
junhyeongleeeee <junhyeong@snaptag.co.kr>
2026-02-06 17:42:23 +0900

    Add missing Text import in App.kt

--- composeApp/src/commonMain/kotlin/com/heisthunt/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/heisthunt/app/App.kt
@@ -3,6 +3,7 @@ package com.heisthunt.app
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.launch
